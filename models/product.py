2cf0c86b8c393d159adfbf49d2ce754aad8d3762
fixxall <faturrohmansugiyarto@gmail.com>
2024-02-05 10:08:02 +0700

    final today

--- models/product.py
+++ models/product.py
@@ -1,6 +1,7 @@
 from extensions import mongo
 from models.auth import User
 from datetime import datetime
+from bson import ObjectId
 
 class Product:
     def get_products_collection():
@@ -27,16 +28,16 @@ class Product:
 
     def buy_product(user_id, product_id):
         users_collection = User.get_users_collection()
-        user = users_collection.find_one({'_id': user_id})
+        user = users_collection.find_one({'_id': ObjectId(user_id)})
 
         if not user:
-            return None  # User not found
+            return "user not found"  # User not found
 
         products_collection = Product.get_products_collection()
-        product = products_collection.find_one({'_id': product_id})
+        product = products_collection.find_one({'_id': ObjectId(product_id)})
 
         if not product:
-            return None  # Product not found
+            return "product not found"  # Product not found
 
         if 'sold' in product and product['sold']:
             return "Product already sold"
@@ -44,18 +45,35 @@ class Product:
         product_price = product['price']
 
         # Check if the user has enough balance to buy the product
-        if 'balance' not in user or user['balance'] < product_price:
-            return "Insufficient balance"
+        if 'balances' not in user or user['balances'] < product_price:
+            return "Insufficient balances"
 
         # Deduct the price from the user's balance
-        new_balance = user['balance'] - product_price
-        users_collection.update_one({'_id': user['_id']}, {'$set': {'balance': new_balance}})
+        new_balance = user['balances'] - product_price
+        buyed_product_info = []
+        if 'buyed_product_info' in user:
+            buyed_product_info = user['buyed_product_info']
+        new_buyed_product_info = {
+            'product_id': product_id,
+            'purchase_date': str(datetime.now())
+        }
+        buyed_product_info.append(new_buyed_product_info)
+        users_collection.update_one({'_id': user['_id']}, {'$set': {'balance': new_balance, 'buyed_product_info':buyed_product_info}})
 
         # Mark the product as sold and record the purchase
         sold_info = {
             'buyer_id': user_id,
-            'purchase_date': datetime.now()
+            'purchase_date': str(datetime.now())
         }
         products_collection.update_one({'_id': product['_id']}, {'$set': {'sold': True, 'sold_info': sold_info}})
 
         return "Purchase successful"
+
+    def get_product_purchased_by_user(user_id):
+        users_collection = User.get_users_collection()
+        user = users_collection.find_one({'_id': ObjectId(user_id)})
+
+        if 'buyed_product_info' not in user:
+            return []
+        else:
+            return user['buyed_product_info']
--- routes/product.py
+++ routes/product.py
@@ -6,6 +6,7 @@ from models import Product  # Assuming you have the Product model
 def product_routes(app, limiter):
     # Router for creating a new product (admin only)
     create_product_parser = reqparse.RequestParser()
+    create_product_parser.add_argument('Authorization', type=str, help='Bearer <your_access_token>', location='headers')
     create_product_parser.add_argument('name', type=str, required=True, help='Product name', location='json')
     create_product_parser.add_argument('price', type=float, required=True, help='Product price', location='json')
     create_product_parser.add_argument('description', type=str, required=True, help='Product description', location='json')
@@ -18,16 +19,16 @@ def product_routes(app, limiter):
         def post(self, current_user):
             try:
                 if not current_user['user']['is_admin']:
-                    app.logger.warning("user "+str(current_user['user']['_id']))
+                    app.logger.warning("user "+str(current_user['user']['id']))
                     return {'message': 'You dont have permission to create product'}, 400
                 data = create_product_parser.parse_args()
-                admin_id = "admin123"  # Replace with the actual admin ID
+                admin_id = current_user['user']['id']  # Replace with the actual admin ID
                 name = data['name']
                 price = data['price']
                 description = data['description']
                 product_id = Product.create_product(admin_id, name, price, description)
                 if product_id:
-                    app.logger.info("user "+str(current_user['user']['_id'])+" successfully created product: "+str(product_id))
+                    app.logger.info("user "+str(current_user['user']['id'])+" successfully created product: "+str(product_id))
                     return jsonify({'message': 'Product created successfully', 'product_id': product_id})
                 else:
                     return {'message': 'Error creating the product'}, 400
@@ -37,23 +38,34 @@ def product_routes(app, limiter):
                 return {'server error':f"Error on {e}"}, 500
 
     # Router for getting all products
+    get_all_parser = reqparse.RequestParser()
+    get_all_parser.add_argument('Authorization', type=str, help='Bearer <your_access_token>', location='headers')
     @app.route('/getall')
     class GetAllProductsResource(Resource):
         @limiter.exempt
+        @app.expect(get_all_parser)
         @verify_access_token
         def get(self, current_user):
             try:
                 all_products = Product.get_all_products()
-                app.logger.info("user "+str(current_user['user']['_id'])+" get all products")
-                return jsonify({'products': [product for product in all_products]})
+                lisdata = []
+                for product in all_products:
+                    dataset = {}
+                    for key, value in product.items():
+                        if(key == '_id'): dataset['id'] = str(value)
+                        else: dataset[key] = value
+                    lisdata.append(dataset)
+                print(lisdata)
+                app.logger.info("user "+str(current_user['user']['id'])+" get all products")
+                return {'products': lisdata}, 200
             except Exception as e:
                 app.logger.error(f"Error on {e}")
                 return {'server error':f"Error on {e}"}, 500
 
     # Router for buying a product (user only)
     buy_product_parser = reqparse.RequestParser()
+    buy_product_parser.add_argument('Authorization', type=str, help='Bearer <your_access_token>', location='headers')
     buy_product_parser.add_argument('product_id', type=str, required=True, help='Product ID', location='json')
-
     @app.route('/buy')
     class BuyProductResource(Resource):
         @limiter.limit("5 per minute")
@@ -62,16 +74,41 @@ def product_routes(app, limiter):
         def post(self, current_user):
             try:
                 data = buy_product_parser.parse_args()
-                user_id = "user456"  # Replace with the actual user ID
+                user_id = current_user['user']['id']
                 product_id = data['product_id']
 
                 result = Product.buy_product(user_id, product_id)
                 if result == "Purchase successful":
-                    app.logger.info("user "+str(current_user['user']['_id'])+" success on buying item")
+                    app.logger.info("user "+str(current_user['user']['id'])+" success on buying item")
                     return jsonify({'message': 'Product purchased successfully'})
                 else:
-                    app.logger.warning("user "+str(current_user['user']['_id'])+" failed buying item")
+                    app.logger.warning("user "+str(current_user['user']['id'])+" failed buying item")
                     return {'message': result}, 400
+            except Exception as e:
+                app.logger.error(f"Error on {e}")
+                return {'server error':f"Error on {e}"}, 500
+
+    purchased_parser = reqparse.RequestParser()
+    purchased_parser.add_argument('Authorization', type=str, help='Bearer <your_access_token>', location='headers')
+    @app.route('/purchased')
+    class PurchasedResource(Resource):
+        @limiter.limit("1 per second")
+        @app.expect(purchased_parser)
+        @verify_access_token
+        def get(self, current_user):
+            try:
+                user_id = current_user['user']['id']
+
+                all_item = Product.get_product_purchased_by_user(user_id)
+                lisdata = []
+                for product in all_item:
+                    dataset = {}
+                    for key, value in product.items():
+                        if(key == '_id'): dataset['id'] = str(value)
+                        else: dataset[key] = value
+                    lisdata.append(dataset)
+                app.logger.info("user "+str(current_user['user']['id'])+" get purchased products")
+                return {'products': lisdata}, 200
             except Exception as e:
                 app.logger.error(f"Error on {e}")
                 return {'server error':f"Error on {e}"}, 500
